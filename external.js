7f905f593d40b9b7a77db0c673d7eb3c2b9927ff
Gyanashree Giri <giri.gyanashree347@gmail.com>
2022-02-14 17:39:16 +0545

    Select random colors fro  grids on hovering. Add buttons for reset and showing grid lines.

--- external.js
+++ external.js
@@ -1,33 +1,68 @@
-/*
-    objective: draw a square of xXx. x is provided by user
-
-    styling and layout in the css
-
-    creating div in js
-
-    
- */
-
 const container = document.querySelector('.container');
+let colored = true;
 
-
-let row = 100
-for (let i=0; i < (row*row); i++) {
-    //create a new cell
-    container.style.setProperty('--grid-rows', row);
-    container.style.setProperty('--grid-cols', row);
-    let cell = document.createElement("div");
+let squareLen = askForSize();
+createCells();
+function createCells() {
+    for (let i=0; i < (squareLen * squareLen); i++) {
+        container.style.setProperty('--grid-rows', squareLen);
+        container.style.setProperty('--grid-cols', squareLen);
+        let cell = document.createElement("div");       
+        container.appendChild(cell).className = 'grid-item';
+    }
     
-    container.appendChild(cell).className = 'grid-item';
-    //append it
+}
+
+
+function askForSize() {
+    let userAns ;
+    do {
+        userAns = prompt("What will be the length of your square? (not more than 100)");
+        console.log(userAns)
+    } while((userAns < 1) || (userAns > 100 ) || isNaN(userAns));
+    return userAns;
+    
+}
+
+
+let pickColor = () => Math.floor(Math.random() * 16);
+function colorThePixel(cell) {
+    letters= '0123456789ABCDEF'
+    let color = "#";
+    for (let i=0; i<6; i++) {
+        color += letters[pickColor()];
+    }
+    cell.style.backgroundColor = color;
 }
 
 const cells = document.querySelectorAll(".grid-item")
 
-// cells.forEach((cell) => {
-//     () => cell.addEventListener("mouseout", cell.style.backgroundColor = "beige")
-// });
-
 cells.forEach((cell) => {
-    cell.addEventListener("mouseout", () => cell.style.backgroundColor = "beige")
-  });
+    
+    cell.addEventListener("mouseenter", () => {
+        if(colored) colorThePixel(cell);
+        
+    })
+});
+
+const resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener('click', () => {
+    location.reload();
+
+    //while ( container.firstChild ) container.removeChild( container.firstChild );
+    // askForSize();
+    // createCells();
+    //cells.forEach((cell) => cell.style.backgroundColor = 'white');
+});
+
+const gridsBtn = document.querySelector(".show-grids");
+gridsBtn.addEventListener('click', () => {
+    if (gridsBtn.textContent=="Show Grids") {
+        cells.forEach((cell) => cell.style.setProperty('border', '1px solid rgba(0, 0, 0, 0.6)'))
+        gridsBtn.textContent= "Hide Grids";
+    } else {
+        cells.forEach((cell) => cell.style.setProperty('border', '0'))
+        gridsBtn.textContent = "Show Grids";
+    }
+});
+
